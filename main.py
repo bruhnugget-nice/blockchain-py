23311b3f37cdaf692b0a2101c91668b8dfc0485a
bruhnugget <77593158+bruhnugget-nice@users.noreply.github.com>
2024-09-03 20:57:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ import datetime as dt
 import hashlib as hasher
 import json
 import os
+import random
 
 # An array that will store our local JSON data, this is for verifying the blockchain
 jarr = []
 coins_amount = int(input("How many coins? "))
 p=input("Set your password to whatever you want! ")
 
+
 # Set the number of coins
 def set_coins(n):
   coins_amount=n
   return coins_amount
 
+# Set password
 def set_password(password):
   p=password
   return p
 
 def read_blockchain():
-  with open("blockchain.json", 'r', encoding='utf-8')as f:
+  with open("temp_blockchain.json", 'r', encoding='utf-8')as f:
     jsonarr=json.loads(f.read())
     return jsonarr
 
@@ -44,7 +47,7 @@ class Block:
 
 # Add attributes of the block to the array, which will then be added to the JSON file.
 def update_blockchain(b: Block):
-  with open("blockchain.json", 'w', encoding='utf-8') as feedjson:
+  with open("temp_blockchain.json", 'w', encoding='utf-8') as feedjson:
     entry = {
         'index': b.index,
         'timestamp': b.timestamp.strftime("%m/%d/%Y, %H:%M:%S"),
@@ -73,25 +76,39 @@ def create_genesis_block():
 
 # Create a new block, and add it to the blockchain based on the previous one.
 def create_next_block(last_block):
+  global coins_amount
   new_index = last_block.index + 1
   new_timestamp = dt.datetime.now()
   new_data = {
     'to': input(f"Block {new_index}\nWho would you like to send this to? "),
     'from': str(input("Enter your password(a 2-digit number): ")),
-    'amount': input("How much would you like to send? "),
+    'amount': int(input("How much would you like to send? ")),
   }
+  if new_data['amount']>coins_amount:
+    print("You don't have enough coins!")
+    for _ in range(3):
+      print(f"This is your reattempt number: {_+1}")
+      create_next_block(last_block)
+  else:
+    coins_amount-=new_data['amount']
+    print(f"You now have {coins_amount} coins")
   if new_data['from'] != p:
     print("Not authorized user")
-    print(new_data['from'], " is not", p)
+    print(new_data['from'] + " is not " + "the password.")
     exit()
   new_hash = last_block.hash
 
+  choice=input("Would you like to mine coins? [y/n]").lower()
+  if choice=='y':
+    mine_add_block()
+  else:
+    pass
   return Block(new_index, new_timestamp, new_data, new_hash)
 
 
 # See if the blockchain is valid.
 def verify_chain():
-  with open("blockchain.json", 'r', encoding='utf-8') as feedjson:
+  with open("temp_blockchain.json", 'r', encoding='utf-8') as feedjson:
     j = json.load(feedjson)
     for i in range(1, len(j)):
       block = j[i]
@@ -111,20 +128,37 @@ def verify_chain():
       if block['previous_hash'] != prev_block['hash']:
         print("Invalid block hash becasue block prev_hash is not pb hash")
         return False
-      PB = B
 
     print("The blockchain is valid!")
     return True
 
+#Solve a simple math problem and thus add to the number of coins.
 def mine_add_block():
-  if len(jarr)>1:
-    print(jarr[-1])
+  global coins_amount
+  global jarr
+  jarr=read_blockchain()
+  NB=None
+
+  NB=Block(jarr[-1]['index']+1,jarr[-1]['timestamp'],jarr[-1]['data'],jarr[-1]['hash']) 
+
+  num1=random.randint(0,9)
+  num2=random.randint(0,9)
+  print(f"What is {num1} plus {num2}?")
+
+  ans=int(input(""))
+  if ans == num1+num2:
+    coins_amount+=100
+    print(f"The amount of coins now is {coins_amount}")
+  else:
+    pass
+    print("You got the answer wrong!")
+  
 
 # Create a demonstration of the blockchain.
 def demo():
   jsonarr=[]
   #Read jsonarr
-  if os.path.getsize("blockchain.json")==0:
+  if os.path.getsize("temp_blockchain.json")==0:
     pass
   else:
     jsonarr=read_blockchain()
